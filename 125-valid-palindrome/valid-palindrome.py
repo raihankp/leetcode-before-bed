b8665bea6ef6ea484028b0dc179f651d78720da5
Raihan Kus Putranto <raihankp96@gmail.com>
2025-11-13 22:12:55 +0700

    Time: 7 ms (82.54%) | Memory: 18.1 MB (68.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,26 +1,29 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # Bruteforce (hm not really)
         # Convert first by looping and the string and checking whether it's a alphanumeric, if it's alphabet then make it lowercase.
         # Then, do a loop one more time with 2 pointer, one in the first character and one in the last character
         # check if they are the same and check until the second pointer is less than first pointer, then return the result
-        
-        converted = ""
+        # Time complexity is O(n) which is good but the space complexity is O(n) which we could enhance
 
-        for c in s:
-            if c.isdigit():
-                converted += c
-            elif c.isalpha():
-                converted += c.lower()
+        # Better solution:
+        # Like the bruteforce solution but instead of converting first, we could directly loop through the original, and check first if the character is alphanum or not, if not then just skip it
         
         # Use two pointer
         leftPointer = 0
-        rightPointer = len(converted) - 1
+        rightPointer = len(s) - 1
 
         while leftPointer < rightPointer:
-            if converted[leftPointer] != converted[rightPointer]:
-                return False
-            leftPointer += 1
-            rightPointer -= 1
+            if not s[leftPointer].isalnum():
+                leftPointer += 1
+            elif not s[rightPointer].isalnum():
+                rightPointer -= 1
+            else:
+                if str(s[leftPointer]).lower() != str(s[rightPointer]).lower():
+                    return False
+
+                leftPointer += 1
+                rightPointer -= 1
                 
         return True
         
