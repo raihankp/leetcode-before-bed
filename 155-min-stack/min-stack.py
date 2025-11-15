41ba4935bd36c1f502031123acd1363ffef74a50
Raihan Kus Putranto <raihankp96@gmail.com>
2025-11-16 00:45:21 +0700

    Time: 7 ms (50.06%) | Memory: 21.3 MB (89.42%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -8,15 +8,19 @@ class MinStack:
     # Like for example, when we push a digit, there are 2 stack that we need to update, the first stack that would store the stack, and the second stack that would store "what is the minimum value when this digit is being pushed compared to the previous minimum value"
     # For example with stack = [-1, 0, -3, 4, -7], the minStack would be [-1, -1, -3, -3, -7]
     # And don't forget to pop the minStack too when popping the original stack
+    # Time complexity would be O(1)
 
     def __init__(self):
         self.stack = []
         self.minStack = []
         
-
     def push(self, val: int) -> None:
         self.stack.append(val)
-        self.setMin(val)
+
+        if self.minStack == []:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(min(val, self.minStack[-1]))
 
     def pop(self) -> None:
         self.stack.pop()
@@ -28,13 +32,6 @@ class MinStack:
     def getMin(self) -> int:
         return self.minStack[-1]
         
-    def setMin(self, val) -> None:
-        if self.minStack == []:
-            self.minStack.append(val)
-        else:
-            self.minStack.append(min(val, self.minStack[-1]))
-
-
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
