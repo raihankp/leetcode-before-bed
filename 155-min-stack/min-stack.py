400703c4bae84e8637cc57e6311bafcc7af22fe7
Raihan Kus Putranto <raihankp96@gmail.com>
2025-11-16 00:43:51 +0700

    Time: 7 ms (50.06%) | Memory: 21.3 MB (59.62%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,43 @@
+class MinStack:
+    # Bruteforce:
+    # To get the minimum element, we could just loop through the stack array
+    # But time complexity would be O(n)
+
+    # Better solution:
+    # Create 2 stack where 1 is for the stack and 1 is for the minimum value for each node in the stack
+    # Like for example, when we push a digit, there are 2 stack that we need to update, the first stack that would store the stack, and the second stack that would store "what is the minimum value when this digit is being pushed compared to the previous minimum value"
+    # For example with stack = [-1, 0, -3, 4, -7], the minStack would be [-1, -1, -3, -3, -7]
+    # And don't forget to pop the minStack too when popping the original stack
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.setMin(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
+    def setMin(self, val) -> None:
+        if self.minStack == []:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(min(val, self.minStack[-1]))
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
