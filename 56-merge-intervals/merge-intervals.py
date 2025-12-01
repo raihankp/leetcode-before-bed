6203a8232106dee570ea6a5bd62304032e5a22d8
Raihan Kus Putranto <raihankp96@gmail.com>
2025-12-01 23:29:58 +0700

    Time: 0 ms (100.00%) | Memory: 20.9 MB (65.44%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,27 +4,12 @@ class Solution:
         solutionArrays = [intervals[0]]
 
         for i in range(1, len(intervals)):
-            foundOverlap = False
-            for j in range(len(solutionArrays)):
-                # if solutionArrays[j][0] > intervals[i][1] and solutionArrays[j][1] < intervals[i][0]:
-                #     solutionArrays[j][0] = min(solutionArrays[j][0], intervals[i][0])
-                #     solutionArrays[j][1] = max(solutionArrays[j][1], intervals[i][1])
-                #     continue
-                if solutionArrays[j][0] <= intervals[i][1] and solutionArrays[j][1] >= intervals[i][0]: 
-                    newSolution = [0,0]
-                    newSolution[0] = min(solutionArrays[j][0], intervals[i][0])
-                    newSolution[1] = max(solutionArrays[j][1], intervals[i][1])
-                    # solutionArrays[j][0] = min(solutionArrays[j][0], intervals[i][0])
-                    # solutionArrays[j][1] = max(solutionArrays[j][1], intervals[i][1])
-                    if newSolution not in solutionArrays:
-                        solutionArrays[j] = newSolution
-
-                    foundOverlap = True
-                    continue
-                # elif solutionArrays[j][0] < intervals[i][1] and solutionArrays
-            if foundOverlap == False:
+            # Overlap
+            if solutionArrays[-1][1] >= intervals[i][0]:
+                solutionArrays[-1][1] = max(solutionArrays[-1][1], intervals[i][1])
+            else:
                 solutionArrays.append(intervals[i])
-            
+
         return solutionArrays
 
         
